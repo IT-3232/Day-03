eb3ddfa77825b8c111050be64be247f6f56b8f29
Thilankavishka <thilankavishka25@gmail.com>
2025-03-29 01:35:53 +0530

    Create GetMethodname and get names from objects

--- myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lk.ac.vau.fas.ict.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @RestController
 @RequestMapping("/app")
@@ -13,5 +16,12 @@ public class AppController {
     Student ob2 = new Student("2020ict21","Sandeepa",25,"IT",6.0);
 	Student ob3 = new Student("2020ict49","goka",25,"IT",4.0);
 	Student ob4 = new Student("2020ict32","baiyamalliteshan",25,"IT",4.0);
+
+
+    @GetMapping("/name")
+    public String getMethodName() {
+        return ob1.getName();
+    }
+    
 	
 }
