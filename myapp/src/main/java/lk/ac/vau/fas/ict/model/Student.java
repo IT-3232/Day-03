06ce2b640704496971b2a5447fc898374dc34a95
Thilankavishka <thilankavishka25@gmail.com>
2025-03-29 01:23:58 +0530

    Create Student Variables

--- myapp/src/main/java/lk/ac/vau/fas/ict/model/Student.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/model/Student.java
@@ -1,5 +1,10 @@
 package lk.ac.vau.fas.ict.model;
 
 public class Student {
-
+	  private String regNo;
+	   private String name;
+	   private int age;
+	   private String course;
+	   private double gpa;
+	   
 }
